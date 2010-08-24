d89232cd8fd0867936141d459a1798697d993574
Jon Homan <jon@jonhoman.com>
2010-08-23 21:57:27 -0500

    more progress. factored out player class.

---
+++ player.rb
@@ -0,0 +1,17 @@
+class Player
+
+  attr_accessor :piece
+  def initialize(piece)
+    @piece = piece
+  end
+  
+  def make_move
+    print "Player #{piece} choose your move (1-9): "
+    gets.chomp
+  end
+  
+  def to_s
+    @piece
+  end
+  
+end
